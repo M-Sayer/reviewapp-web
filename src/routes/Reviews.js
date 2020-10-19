2ac041a5a45f30e6394037f1a7b21e0ba5ca24f0
Muhaajir Benjamin Sayer <muhaajirsayer@Muhaajirs-MBP.fios-router.home>
2020-10-19 11:04:09 -0400

    adding loading indicator to compensate for heroku sleep on inactive server

--- src/routes/Reviews.js
+++ src/routes/Reviews.js
@@ -1,4 +1,4 @@
-import { Flex, FormLabel, Stack, Switch } from '@chakra-ui/core';
+import { Flex, FormLabel, Stack, Switch, Text } from '@chakra-ui/core';
 import React, { useEffect, useState } from 'react';
 import { ReviewCard } from '../components/reviewCard';
 import { getReviews } from '../services/apiService';
@@ -7,6 +7,7 @@ export const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   const [points, setPoints] = useState(false);
   const [sortComments, setSortComments] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   //using to refetch when updating points in child component
   const toggle = () => setPoints(!points); 
@@ -15,6 +16,7 @@ export const Reviews = () => {
     async function getData() {
       const data = await getReviews();
       data && setReviews(data);
+      setLoading(false);
     }
 
     getData();
@@ -33,15 +35,20 @@ export const Reviews = () => {
   
   return (
     <Flex direction='column' align='center'>
-      <h1>REVIEWS</h1>
+      {!loading && <h1>REVIEWS</h1>}
       <Stack align='center' spacing={8} mb={10}>
       <Flex ml='auto' mr='20'>
-        <FormLabel htmlFor="email-alerts">show new comments first?</FormLabel>
-        <Switch 
-          id="email-alerts" 
-          isChecked={sortComments}
-          onChange={() => setSortComments(!sortComments)}
-        />
+        {loading && <Text>Loading...</Text>}
+        {reviews.length > 0 && 
+          <>
+            <FormLabel htmlFor="email-alerts">show new comments first?</FormLabel>
+            <Switch 
+              id="email-alerts" 
+              isChecked={sortComments}
+              onChange={() => setSortComments(!sortComments)}
+            />
+          </>
+        }
       </Flex>
         {renderReviews()}
       </Stack>
